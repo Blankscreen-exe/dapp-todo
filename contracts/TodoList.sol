b4aa4346abf0eb7be444424c0a165074daad1178
Blankscreen-exe <blazinginpachi1850.0@gmail.com>
2023-11-14 18:18:02 +0500

    add frontend assets

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,22 @@ pragma solidity ^0.5.0;
 
 contract TodoList{
     uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    // acts as a database for key-value pairs
+    mapping (uint => Task) public tasks;
+
+    constructor() public {
+        createTask("DOOP");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
